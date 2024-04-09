d01f1aad843e1a01c8e1fe9b36a71aac36acc952
LuisFernandoLG <luislopez1099lg@gmail.com>
2024-04-09 11:53:21 -0700

    style: :art: code

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import ReduxProvider from "@/components/ReduxProvider";
 import { ThemeProvider } from "@/components/theme-provider";
-import {NextIntlClientProvider, useMessages} from 'next-intl';
+import { NextIntlClientProvider, useMessages } from "next-intl";
 import QueryProvider from "@/components/QueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,46 +16,49 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  params: {locale}
+  params: { locale },
 }: {
   children: React.ReactNode;
-  params: {locale: string};
+  params: { locale: string };
 }) {
   const messages = useMessages();
 
   return (
     <html lang={locale}>
       <body className={`${inter.className}  min-h-dvh bg-background`}>
-      <NextIntlClientProvider messages={messages}>
-      <ReduxProvider>
-<QueryProvider>
-
-      <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-            >
-
-      <Toaster position="top-center" toastOptions={{ 
-          classNames: {
-            description: 'group-[.toast]:text-muted-foreground',
-            actionButton: 'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
-            cancelButton: 'group-[.toast]:bg-white group-[.toast]:text-black',
-            error:
-              'group toast group-[.toaster]:bg-red group-[.toaster]:text-red-600 dark:group-[.toaster]:text-foreground group-[.toaster]:shadow-lg',
-            success:
-              'group toast group-[.toaster]:bg-green group-[.toaster]:text-green-600 dark:group-[.toaster]:text-foreground group-[.toaster]:shadow-lg',
-            warning:
-              'group toast group-[.toaster]:bg-yellow group-[.toaster]:text-yellow-600 dark:group-[.toaster]:text-foreground group-[.toaster]:shadow-lg',
-            info: 'group toast group-[.toaster]:bg-blue group-[.toaster]:text-blue-600 dark:group-[.toaster]:text-foreground group-[.toaster]:shadow-lg',
-          },
-       }} />
-      {children}
-          </ThemeProvider>
+        <NextIntlClientProvider messages={messages}>
+          <ReduxProvider>
+            <QueryProvider>
+              <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+              >
+                <Toaster
+                  position="top-center"
+                  toastOptions={{
+                    classNames: {
+                      description: "group-[.toast]:text-muted-foreground",
+                      actionButton:
+                        "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+                      cancelButton:
+                        "group-[.toast]:bg-white group-[.toast]:text-black",
+                      error:
+                        "group toast group-[.toaster]:bg-red group-[.toaster]:text-red-600 dark:group-[.toaster]:text-foreground group-[.toaster]:shadow-lg",
+                      success:
+                        "group toast group-[.toaster]:bg-green group-[.toaster]:text-green-600 dark:group-[.toaster]:text-foreground group-[.toaster]:shadow-lg",
+                      warning:
+                        "group toast group-[.toaster]:bg-yellow group-[.toaster]:text-yellow-600 dark:group-[.toaster]:text-foreground group-[.toaster]:shadow-lg",
+                      info: "group toast group-[.toaster]:bg-blue group-[.toaster]:text-blue-600 dark:group-[.toaster]:text-foreground group-[.toaster]:shadow-lg",
+                    },
+                  }}
+                />
+                {children}
+              </ThemeProvider>
             </QueryProvider>
-      </ReduxProvider>
-            </NextIntlClientProvider>
+          </ReduxProvider>
+        </NextIntlClientProvider>
       </body>
     </html>
   );

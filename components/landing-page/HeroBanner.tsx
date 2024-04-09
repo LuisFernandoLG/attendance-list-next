4fe33401daeea381bdf2c30aa50ba45d54f2b22b
LuisFernandoLG <luislopez1099lg@gmail.com>
2024-04-09 13:09:01 -0700

    feat: :passport_control: translation added to homepage

--- components/landing-page/HeroBanner.tsx
+++ components/landing-page/HeroBanner.tsx
@@ -1,13 +1,15 @@
 import { ArrowRightIcon } from "@radix-ui/react-icons";
 import rec from "./../../assets/images/rectangleQr.png"
 import square from "./../../assets/images/squareQr.png"
+import { useTranslations } from "next-intl";
 
 export  function HeroBanner(){
+  const t = useTranslations('Index');
   
   return <section className="flex flex-col xl:flex-row gap-5 mx-auto max-w-[1200px] mt-10 p-5">
     <article>
-      <h1 id="homepage-title" className="text-5xl text-center xl:text-left font-black bg-gradient-to-r from-purple-700 dark:to-white to-black">¡Tu herramienta de pase de lista!</h1>
-      <p className="mt-5 px-5 sm:p-0 text-base sm:text-xl text-center sm:text-left ">Toma el control de las personas que asisten a tus <span className="text-violet-700 font-bold">clases</span>, <span className="text-rose-700 font-bold">conferencias</span>, <span className="text-purple-700-700 font-bold">reuniones</span>, <span className="font-bold">etc.</span></p>
+      <h1 id="homepage-title" className="text-5xl text-center xl:text-left font-black bg-gradient-to-r from-purple-700 dark:to-white to-black">{t("title")}</h1>
+      <p className="mt-5 px-5 sm:p-0 text-base sm:text-xl text-center sm:text-left ">{t("description")} <span className="text-violet-700 font-bold underline">{t("event1")}</span>, <span className="text-rose-700 font-bold underline">{t("event2")}</span>, <span className="text-purple-700-700 font-bold underline">{t("event3")}</span>, <span className="font-bold">etc.</span></p>
       <span className="flex flex-col xl:flex-row items-center justify-center xl:justify-start font-bold text-center mt-5 xl:text-left gap-2">Escanea alguno de los siguientes códigos <ArrowRightIcon className="ml-2 size-5 rotate-90 xl:rotate-0"/></span>
     </article>
     <article className="flex flex-row items-start  w-max-[800px] gap-5 justify-center p-5">
